0756098beb77ce1327be36b5e30c5d85880e815b
Sinistra Chengjun Pan <sinistrapan@gmail.com>
2026-03-30 18:33:18 -0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #
+set -e
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
@@ -21,3 +22,9 @@ git clone https://github.com/eamonxg/luci-theme-aurora package/luci-theme-aurora
 git clone https://github.com/eamonxg/luci-app-aurora-config package/luci-app-aurora-config
 git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
 git clone https://github.com/timsaya/openwrt-bandix package/openwrt-bandix
+
+# HomeProxy
+git clone https://github.com/immortalwrt/homeproxy.git package/homeproxy
+
+# Xray LuCI app
+git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
